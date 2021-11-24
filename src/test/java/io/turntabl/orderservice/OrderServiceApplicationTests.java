55144c6053e809d1c706a0e261bc82e1b1be449b
Kwame Gyimah <kwame.owusu-gyimah@turntabl.io>
2021-11-24 14:58:13 +0000

    updated pom file

--- src/test/java/io/turntabl/orderservice/OrderServiceApplicationTests.java
+++ src/test/java/io/turntabl/orderservice/OrderServiceApplicationTests.java
@@ -1,13 +1,17 @@
 package io.turntabl.orderservice;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class OrderServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 
 }
