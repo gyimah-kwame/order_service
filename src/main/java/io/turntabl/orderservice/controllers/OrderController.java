b744c24c5144d3cbfed103b2b74f648c4b258ba7
Kwame Gyimah <kwame.owusu-gyimah@turntabl.io>
2021-12-01 19:00:50 +0000

    configured security

--- src/main/java/io/turntabl/orderservice/controllers/OrderController.java
+++ src/main/java/io/turntabl/orderservice/controllers/OrderController.java
@@ -4,7 +4,10 @@ import io.turntabl.orderservice.dtos.OrderDto;
 import io.turntabl.orderservice.requests.OrderRequest;
 import io.turntabl.orderservice.services.OrderService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -14,21 +17,21 @@ import java.util.List;
 @RequestMapping("/api/v1")
 @CrossOrigin
 @AllArgsConstructor
+@Slf4j
 public class OrderController {
 
     private final OrderService orderService;
 
     @PostMapping("/orders")
     @ResponseStatus(code = HttpStatus.CREATED)
-    public OrderDto createOrder(@Valid @RequestBody OrderRequest orderRequest){
-        return orderService.createOrder("1", orderRequest);
+    public OrderDto createOrder(@Valid @RequestBody OrderRequest orderRequest, @AuthenticationPrincipal Jwt principal) {
+        return orderService.createOrder(principal.getSubject(), orderRequest);
     }
 
     @GetMapping("/orders")
-    public List<OrderDto> getOrders(){
-        return orderService.getAllOrders("1");
+    public List<OrderDto> getOrders( @AuthenticationPrincipal Jwt principal) {
+        return orderService.getAllOrders(principal.getSubject());
     }
-
 
 
 }
