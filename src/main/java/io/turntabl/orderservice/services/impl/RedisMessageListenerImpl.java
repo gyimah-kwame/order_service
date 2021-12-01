1e729b350688110c11b8b717aace6b51da77b7ed
Kwame Gyimah <kwame.owusu-gyimah@turntabl.io>
2021-12-01 20:27:18 +0000

    temp

--- src/main/java/io/turntabl/orderservice/services/impl/RedisMessageListenerImpl.java
+++ src/main/java/io/turntabl/orderservice/services/impl/RedisMessageListenerImpl.java
@@ -1,5 +1,9 @@
 package io.turntabl.orderservice.services.impl;
 
+import com.google.gson.Gson;
+import io.turntabl.orderservice.constants.OrderStatus;
+import io.turntabl.orderservice.constants.Side;
+import io.turntabl.orderservice.dtos.MarketDataDto;
 import io.turntabl.orderservice.dtos.OrderDto;
 import io.turntabl.orderservice.models.Order;
 import io.turntabl.orderservice.repositories.OrderRepository;
@@ -7,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.core.HashOperations;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -17,6 +22,13 @@ public class RedisMessageListenerImpl implements MessageListener {
 
     @Autowired
     private OrderRepository orderRepository;
+
+    @Autowired
+    private Gson gson;
+
+    @Autowired
+    private HashOperations<String, String, String> hashOperations;
+
 
     @Override
     public void onMessage(Message message, byte[] pattern) {
@@ -30,7 +42,26 @@ public class RedisMessageListenerImpl implements MessageListener {
 
         Order receivedOrder = order.get();
 
-        //perform limit checking
+        String marketDataDtoStr = hashOperations.get(receivedOrder.getTicker(), receivedOrder.getTicker());
+
+        MarketDataDto marketDataDto = gson.fromJson(marketDataDtoStr, MarketDataDto.class);
+
+        log.info("data from hash {} ",marketDataDto);
+
+        //perform limit checking on buy
+        if (receivedOrder.getSide() == Side.BUY && receivedOrder.getQuantity() > marketDataDto.getBuyLimit()) {
+            return;
+        }
+
+        if (receivedOrder.getSide() == Side.SELL && receivedOrder.getQuantity() > marketDataDto.getSellLimit()) {
+            return;
+        }
+
+        double price = receivedOrder.getPrice() * receivedOrder.getQuantity();
+
+        if (receivedOrder.getSide() == Side.BUY) {
+
+        }
 
         //if side is buy, check if user has enough funds to place the order
 
