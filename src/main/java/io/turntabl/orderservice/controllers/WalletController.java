425c40502d9b8a7320fcf82190126c23afe492e5
Kwame Gyimah <kwameowusugyimah@gmail.com>
2021-12-07 12:48:31 +0000

    endpoint to retrieve user portfolios

--- src/main/java/io/turntabl/orderservice/controllers/WalletController.java
+++ src/main/java/io/turntabl/orderservice/controllers/WalletController.java
@@ -7,13 +7,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/")
+@CrossOrigin
 @AllArgsConstructor
 @Slf4j
 public class WalletController {
